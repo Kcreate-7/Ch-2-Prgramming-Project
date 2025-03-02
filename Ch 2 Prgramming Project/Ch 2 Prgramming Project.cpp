58bd2071159587c4ec14c1221bf8f4f33a419d25
kylebell7799@gmail.com <kylebell7799@gmail.com>
2025-03-02 05:33:28 -0500

    energy drink consumption

--- Ch 2 Prgramming Project/Ch 2 Prgramming Project.cpp
+++ Ch 2 Prgramming Project/Ch 2 Prgramming Project.cpp
@@ -8,24 +8,19 @@ using namespace std;
 int main()
 
 {
-    int i, y5, y7, y10;
-    float increase, d5, d7, d10;
-    
-    increase = 1.5;
+    int cust, fans, citrus;
+  
+    cust = 16500;
 
-    y5 = 5;
-    y7 = 7;
-    y10 = 10;
+    fans = cust * .15;
 
-    d5 = increase * y5;
-    d7 = increase * y7;
-    d10 = increase * y10;
+    citrus = fans * .58;
 
-    cout << "In " << y5 << " years, the ocean will have risen " << d5 << " mm higher than today's current level!" << "\n";
+
+    cout << "Out of a survey of " << cust << " people, approximately " << fans << " people consume one or more energy drinks per week." << "\n";
     cout << "\n";
-    cout << "In " << y7 << " years, the ocean will have risen " << d7 << " mm higher than today's current level!" << "\n";
+    cout << "Out of those " << cust << " customers, roughly " << citrus << " people prefer the citrus flavored energy drinks. " << "\n";
     cout << "\n";
-    cout << "In " << y10 << " years, the ocean will have risen " << d10 << " mm higher than today's current level!" << "\n";
 
 
     return 0;
